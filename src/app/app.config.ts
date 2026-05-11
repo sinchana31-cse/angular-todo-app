071d8fa37020f83dcb9051fda7b611f978aab959
decem312004 <Sinchana312004@gmail.com>
2026-05-11 22:07:18 +0530

    Completed colorful Angular Todo App

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,1 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
-};
+export {};
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,99 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('angular-todo-app');
+export class App implements OnInit {
+  task = '';
+
+  darkMode = false;
+
+  editIndex: number | null = null;
+
+  tasks: {
+    text: string;
+    completed: boolean;
+  }[] = [];
+
+  ngOnInit(): void {
+    const savedTasks =
+      localStorage.getItem('tasks');
+
+    if (savedTasks) {
+      this.tasks = JSON.parse(savedTasks);
+    }
+
+    const savedTheme =
+      localStorage.getItem('darkMode');
+
+    if (savedTheme) {
+      this.darkMode = JSON.parse(savedTheme);
+    }
+  }
+
+  addTask() {
+    if (this.task.trim() !== '') {
+
+      if (this.editIndex !== null) {
+
+        this.tasks[this.editIndex].text =
+          this.task;
+
+        this.editIndex = null;
+
+      } else {
+
+        this.tasks.push({
+          text: this.task,
+          completed: false,
+        });
+
+      }
+
+      this.task = '';
+
+      this.saveTasks();
+    }
+  }
+
+  editTask(index: number) {
+    this.task = this.tasks[index].text;
+
+    this.editIndex = index;
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+
+    this.saveTasks();
+  }
+
+  toggleTask(index: number) {
+    this.tasks[index].completed =
+      !this.tasks[index].completed;
+
+    this.saveTasks();
+  }
+
+  toggleTheme() {
+    this.darkMode = !this.darkMode;
+
+    localStorage.setItem(
+      'darkMode',
+      JSON.stringify(this.darkMode)
+    );
+  }
+
+  saveTasks() {
+    localStorage.setItem(
+      'tasks',
+      JSON.stringify(this.tasks)
+    );
+  }
 }
